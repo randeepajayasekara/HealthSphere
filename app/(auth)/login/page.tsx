63ad379effe4330d65f451adb901806e410b970e
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-06-19 19:16:13 +0530

    New: Login Page Base0

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,31 +1,23 @@
-import { LoginForm } from "../../components/layout/Auth/login-form";
-import { motion } from "framer-motion";
+import Image from "next/image"
+import { LoginForm } from "@/app/components/layout/Auth/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 bg-[url('https://i.postimg.cc/0NQtJ4cp/hall-bg.jpg')] bg-cover bg-center bg-no-repeat">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <span className="flex items-center gap-2 self-center font-medium text-black dark:text-white select-none">
-          <div className="flex items-center justify-center rounded-md bg-transparent text-black dark:text-white">
-            <img
-              src="https://i.ibb.co/T9pQJr6/icon.png"
-              className="w-8 h-auto select-none"
-            />
-          </div>
-          Streamline Cinemas
-        </span>
-        <motion.div
-          initial={{ opacity: 0.0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-        >
-          <LoginForm />
-        </motion.div>
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
+          <Image
+            src="/icon0.svg"
+            width={32}
+            height={32}
+            priority
+            alt="Healthsphere"
+            className="w-8 h-auto select-none"
+          />
+          
+        </a>
+        <LoginForm />
       </div>
     </div>
-  );
+  )
 }
